bb3ae533998e18d1d084efbe49b1969386fb39d8
Marlon Wiprud <marlonwiprud1@gmail.com>
2022-02-02 14:10:12 -0500

    set up web3 connector class

---
+++ src/web3Connectors/index.tsx
@@ -0,0 +1,47 @@
+import { InjectedConnector } from "@web3-react/injected-connector";
+import { PortisConnector } from "@web3-react/portis-connector";
+import { FortmaticConnector } from "@web3-react/fortmatic-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
+
+export const CHAIN_ID_MAIN_NET = 1;
+export const CHAIN_ID_ROPSTEN = 3;
+
+export const SUPPORTED_CHAIN_IDS = [CHAIN_ID_MAIN_NET, CHAIN_ID_ROPSTEN];
+
+export class Web3Connectors {
+  supportedChainIDs: number[];
+  activeChainID: number;
+
+  injected: InjectedConnector;
+  formatic: FortmaticConnector;
+  walletConnect: WalletConnectConnector;
+  portis: PortisConnector;
+
+  constructor(
+    fortmaticApiKey: string,
+    portisDappID: string,
+    defaultChainID: number,
+    supportedChainIDs: number[] = SUPPORTED_CHAIN_IDS
+  ) {
+    this.supportedChainIDs = supportedChainIDs;
+    this.activeChainID = defaultChainID;
+
+    this.injected = new InjectedConnector({
+      supportedChainIds: this.supportedChainIDs,
+    });
+
+    this.formatic = new FortmaticConnector({
+      apiKey: fortmaticApiKey,
+      chainId: this.activeChainID,
+    });
+
+    this.walletConnect = new WalletConnectConnector({
+      supportedChainIds: this.supportedChainIDs,
+    });
+
+    this.portis = new PortisConnector({
+      dAppId: portisDappID,
+      networks: this.supportedChainIDs,
+    });
+  }
+}
