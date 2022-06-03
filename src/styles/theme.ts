cbf5caab3d308d82ffc963d84a453a6e39b692bb
basitmir <iammirbasit@gmail.com>
2022-06-03 16:38:16 +0530

    add page background in theme

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,50 +1,52 @@
 import {
-  white,
-  grey,
-  crimson,
-  dodgerBlue,
-  orange,
-  mistyRose,
-  aliceBlue,
-  darkNavy,
-} from ".";
-import { lavendar, lightGray } from "./colors";
+	white,
+	grey,
+	crimson,
+	dodgerBlue,
+	orange,
+	mistyRose,
+	aliceBlue,
+	darkNavy,
+} from '.';
+import { ghostWhite, lavendar, lightGray } from './colors';
 
 interface PaletteBase {
-  primary: string;
-  secondary: string;
-  highlight: string;
-  error: string;
-  warning: string;
+	primary: string;
+	secondary: string;
+	highlight: string;
+	error: string;
+	warning: string;
 }
 
 interface PaletteBackground extends PaletteBase {
-  lowLight: string;
-  tertiary: string;
+	lowLight: string;
+	tertiary: string;
+	page: string;
 }
 
 export type Theme = {
-  background: PaletteBackground;
-  text: PaletteBase;
+	background: PaletteBackground;
+	text: PaletteBase;
 };
 
 export const lightTheme: Theme = {
-  background: {
-    primary: white,
-    secondary: aliceBlue,
-    tertiary: lightGray,
-    highlight: lavendar,
-    lowLight: darkNavy,
-    error: mistyRose,
-    warning: orange,
-  },
-  text: {
-    primary: "black",
-    secondary: grey,
-    highlight: dodgerBlue,
-    error: crimson,
-    warning: orange,
-  },
+	background: {
+		primary: white,
+		secondary: aliceBlue,
+		tertiary: lightGray,
+		highlight: lavendar,
+		lowLight: darkNavy,
+		error: mistyRose,
+		warning: orange,
+		page: ghostWhite,
+	},
+	text: {
+		primary: 'black',
+		secondary: grey,
+		highlight: dodgerBlue,
+		error: crimson,
+		warning: orange,
+	},
 };
 
 /**
