64a4f244bba2e872fb195fbf969a6010cfc55950
Diego Gonzalez <diego.ags04@gmail.com>
2022-07-14 15:27:41 -0400

    chore: support mumbai

--- src/web3Connectors/index.tsx
+++ src/web3Connectors/index.tsx
@@ -6,12 +6,14 @@ import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 export const CHAIN_ID_MAIN_NET = 1;
 export const CHAIN_ID_ROPSTEN = 3;
 export const CHAIN_ID_GOERLI = 5;
+export const CHAIN_ID_MUMBAI = 80001
 export const CHAIN_ID_HARDHAT = 31337;
 
 export const SUPPORTED_CHAIN_IDS = [
   CHAIN_ID_MAIN_NET,
   CHAIN_ID_ROPSTEN,
   CHAIN_ID_GOERLI,
+  CHAIN_ID_MUMBAI,
   CHAIN_ID_HARDHAT,
 ];
 
