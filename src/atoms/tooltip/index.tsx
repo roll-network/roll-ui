7e7e6ef9bdbbdc218fa998f5e9f7dfa34ed168f5
Diego Gonzalez <diego.ags04@gmail.com>
2022-08-04 14:30:49 -0400

    feat: Add styles for membership tooltips

--- src/atoms/tooltip/index.tsx
+++ src/atoms/tooltip/index.tsx
@@ -8,6 +8,7 @@ export type Props = {
   children: React.ReactElement;
   title: string | JSX.Element;
   open?: boolean;
+  isMembership?: boolean;
   placement?:
     | "bottom-end"
     | "bottom-start"
@@ -31,17 +32,40 @@ const LightTooltip = styled(({ className, ...props }: TooltipProps) => (
     color: "rgba(0, 0, 0, 0.87)",
     boxShadow: theme.shadows[1],
     fontSize: 11,
+  }
+}));
+const MembershipTooltip = styled(({ className, ...props }: TooltipProps) => (
+  <MuiTooltip {...props} classes={{ popper: className }} arrow />
+))(({ theme }) => ({
+  [`& .${tooltipClasses.tooltip}`]: {
+    backgroundColor: "#293354",
+    color: "white",
+    fontWeight: 600,
+    boxShadow: "0px 4px 12px rgba(0, 0, 0, 0.1);",
+    maxWidth: 320,
+    padding: 16,
+    fontSize: 16,
+    borderRadius: 16,
   },
+  [`& .${tooltipClasses.arrow}`]: {
+    color: "#293354",
+  }
 }));
 
 export const ToolTip: React.FC<Props> = ({
   children,
-  title,
-  placement,
-  open,
+  isMembership = false,
+  ...props
 }) => {
+  if (isMembership) {
+    return (
+      <MembershipTooltip {...props}>
+        {children}
+      </MembershipTooltip>  
+    )
+  }
   return (
-    <LightTooltip open={open} placement={placement} title={title}>
+    <LightTooltip {...props}>
       {children}
     </LightTooltip>
   );
