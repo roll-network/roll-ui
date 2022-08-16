11c5a36f57f8de0eda6f91fa5e84712b1c886746
Teimur Gasanov <teymurgg321@gmail.com>
2022-08-16 11:47:22 +0200

    chore: update stories

--- src/atoms/typography/typography.stories.tsx
+++ src/atoms/typography/typography.stories.tsx
@@ -1,26 +1,48 @@
 import * as Typography from ".";
-import { titleBuilder } from "../../../.storybook/utils";
+import { titleBuilder, fromTemplate } from "../../../.storybook/utils";
 
 const storyConfig = {
   title: titleBuilder.atoms("Typography"),
   component: Typography.TypographyBase,
 };
 
-export const SubCaption = () => (
-  <Typography.SubCaption>SubCaption</Typography.SubCaption>
-);
-export const Caption = () => <Typography.Caption>Caption</Typography.Caption>;
-export const Body = () => <Typography.Body>Body</Typography.Body>;
-export const SubHeader = () => (
-  <Typography.SubHeader>SubHeader</Typography.SubHeader>
-);
-export const Header = () => <Typography.Header>Header</Typography.Header>;
-export const LargeHeader = () => (
-  <Typography.LargeHeader>LargeHeader</Typography.LargeHeader>
-);
-export const Title = () => <Typography.Title>Title</Typography.Title>;
-export const LargeTitle = () => (
-  <Typography.LargeTitle>LargeTitle</Typography.LargeTitle>
-);
+const getTemplate =
+  (
+    componentName:
+      | "SubCaption"
+      | "Caption"
+      | "Body"
+      | "SubHeader"
+      | "Header"
+      | "LargeHeader"
+      | "Title"
+      | "LargeTitle"
+  ) =>
+  (props: Typography.TypographyBaseProps) => {
+    const Component = Typography[componentName];
+
+    return <Component {...props} />;
+  };
+
+export const SubCaption = fromTemplate(getTemplate("SubCaption"), {
+  children: "SubCaption",
+});
+export const Caption = fromTemplate(getTemplate("Caption"), {
+  children: "Caption",
+});
+export const Body = fromTemplate(getTemplate("Body"), { children: "Body" });
+export const SubHeader = fromTemplate(getTemplate("SubHeader"), {
+  children: "SubHeader",
+});
+export const Header = fromTemplate(getTemplate("Header"), {
+  children: "Header",
+});
+export const LargeHeader = fromTemplate(getTemplate("LargeHeader"), {
+  children: "LargeHeader",
+});
+export const Title = fromTemplate(getTemplate("Title"), { children: "Title" });
+export const LargeTitle = fromTemplate(getTemplate("LargeTitle"), {
+  children: "LargeTitle",
+});
 
 export default storyConfig;
